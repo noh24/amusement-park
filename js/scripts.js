b685496a2d24912231946830267431f453644e61
Brian Noh <noh24@ymail.com>
2022-10-19 19:00:33 -0400

    Add if statement for error-messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function hideResultsAndError() {
   document.getElementById("error-message").setAttribute("class", "hidden");
-  document.getElementById("swing").setAttribute("class", "hidden");
+  document.getElementById("swings").setAttribute("class", "hidden");
   document.getElementById("coaster").setAttribute("class", "hidden");
   document.getElementById("tower").setAttribute("class", "hidden");
   document.getElementById("sorry").setAttribute("class", "hidden");
@@ -8,26 +8,30 @@ function hideResultsAndError() {
 
 window.onload = function() {
   document.querySelector("form").onsubmit = function(e) {
-    e.preventDefault;
+    e.preventDefault();
     hideResultsAndError();
     const age = parseInt(document.querySelector("input#age").value);
     const height = parseInt(document.querySelector("input#height").value);
-    
-    //tower of doom requirements
-    if (age >= 12 && height >= 60) {
-      document.getElementById("swing").removeAttribute("class");
-      document.getElementById("coaster").removeAttribute("class");
-      document.getElementById("tower").removeAttribute("class");
-    //coaster requirements
-    } else if (age >= 12 || height >= 48) { 
-      document.getElementById("swing").removeAttribute("class");
-      document.getElementById("coaster").removeAttribute("class");
-    //swing requirements
-    } else if (age >= 6) {
-      document.getElementById("swing").removeAttribute("class");
-      //catch all else: too short and young
+
+    if (!age && !height) {
+      document.getElementById("error-message").removeAttribute("class");
     } else {
-      document.getElementById("sorry").removeAttribute("class");
+    //tower of doom requirements
+      if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+      //coaster requirements
+      } else if (age >= 12 || height >= 48) { 
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+      //swing requirements
+      } else if (age >= 6) {
+        document.getElementById("swings").removeAttribute("class");
+        //catch all else: too short and young
+      } else {
+        document.getElementById("sorry").removeAttribute("class");
+      }
     }
   };
 };
